568592219457f2fd69379368c5602dff1874f738
Mahmud Adegboyega <129618709+MahmudChiv@users.noreply.github.com>
2025-10-26 12:11:13 +0100

    Update: db config

--- config/database.ts
+++ config/database.ts
@@ -4,9 +4,13 @@ import { Country } from "../models/Country";
 import dotenv from "dotenv";
 dotenv.config();
 
+const dbURL = process.env.DB_URL;
 
+if (!dbURL) {
+  throw new Error("❌ DATABASE_URL is not defined in environment variables!");
+}
 
-export const sequelize = new Sequelize(process.env.DB_URL!, {
+export const sequelize = new Sequelize(dbURL, {
   dialect: "mysql",
   logging: false,
   models: [Country],
